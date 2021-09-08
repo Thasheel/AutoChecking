576f7d467a60d4e7a9c211d30302f3d2f2e080a9
Mohammed Thasheel <spidy9741@gmail.com>
2021-09-08 21:08:00 +0530

    checked the condition

--- autocheck.py
+++ autocheck.py
@@ -4,3 +4,13 @@ femalefile = open("female.txt", "w")
 others = open("others.txt", "w")
 for line in fp:
     words=line.split( )
+    if words[1]=="F":
+        femalefile.write(line)
+    if words[1]=="M":
+        malefile.write(line)
+    if words[1]=="O":
+        others.write(line)
+
+        
+
+
